f9684e6ad64678123bd40e6662342202413248a6
Artem Prokop <artem13.prokop@gmail.com>
2024-07-21 11:09:41 +0200

    fix: use i18n to handle keyboard buttons

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -1,6 +1,6 @@
 import { Bot as TelegramBot, session } from 'grammy';
 import type { Composer, NextFunction } from 'grammy';
-import type { I18n } from '@grammyjs/i18n/dist/source/i18n.js';
+import type { I18n } from '@grammyjs/i18n';
 
 import type { Bot, Middleware } from '../types/telegram.js';
 import type { Database } from '../types/database.js';
@@ -10,7 +10,7 @@ import { playerContext } from '../middlewares/player.js';
 import { defaultContext } from '../middlewares/default-context.js';
 import { resolvePath } from '../helpers/resolve-path.js';
 import { startController } from '../controllers/start.js';
-import { profilesController } from '../controllers/profiles.js';
+import { profileController } from '../controllers/profiles.js';
 import { classController } from '../controllers/class.js';
 import { initLocaleEngine } from './locale-engine.js';
 
@@ -31,12 +31,12 @@ function setupBot(bot: Bot, i18n: I18n) {
   bot.catch(console.error);
 }
 
-function setupControllers(bot: Bot, db: Database, i18n: I18n) {
+function setupControllers(bot: Bot, db: Database) {
   attach(bot, defaultContext(db));
   attach(bot, startController);
-  attach(bot, classController(i18n));
+  attach(bot, classController);
   attach(bot, playerContext);
-  attach(bot, profilesController(i18n));
+  attach(bot, profileController);
   attach(bot, warShutdown);
 }
 
@@ -47,7 +47,7 @@ export async function startBot(database: Database): Promise<Bot> {
   const bot = new TelegramBot<DefaultContext>(process.env.TOKEN);
 
   setupBot(bot, i18n);
-  setupControllers(bot, database, i18n);
+  setupControllers(bot, database);
 
   return new Promise(resolve => bot.start({ onStart: () => resolve(bot) }));
 }
--- src/controllers/class.ts
+++ src/controllers/class.ts
@@ -1,46 +1,47 @@
 import { Composer, InputFile } from 'grammy';
-import type { I18n } from '@grammyjs/i18n';
+import { hears } from '@grammyjs/i18n';
 
+import type { Player } from '../types/database.js';
 import type { DefaultContext } from '../types/context.js';
 import { createPlayer, getPlayer } from '../services/player.js';
 import { resolvePath } from '../helpers/resolve-path.js';
 import { getRandomInt } from '../helpers/randomInteger.js';
 
-export function classController(i18n: I18n) {
-  const controller = new Composer<DefaultContext>();
+async function handleClassSelection(ctx: DefaultContext, classType: Player['classType']) {
+  const player = await getPlayer({ db: ctx.db, userId: ctx.from.id });
+  if (player) {
+    return;
+  }
 
-  const gnomeButton = i18n.t('en', 'buttons.class.gnomes');
-  const knightButton = i18n.t('en', 'buttons.class.knights');
+  const emoji = classType === 'gnome' ? getRandomInt(1, 3) : getRandomInt(4, 6);
 
-  controller.chatType('private').hears([gnomeButton, knightButton], async ctx => {
-    const player = await getPlayer({ db: ctx.db, userId: ctx.from.id });
-    if (player) {
-      return;
-    }
-
-    const classType = ctx.message.text === gnomeButton ? 'gnome' : 'knight';
-    const emoji = classType === 'gnome' ? getRandomInt(1, 3) : getRandomInt(4, 6);
-
-    await createPlayer({
-      db: ctx.db,
-      data: {
-        userId: ctx.from.id,
-        name: ctx.from.username ?? 'Unnamed',
-        emoji,
-        gold: 0,
-        classType,
-        state: 'normal',
-      },
-    });
-
-    await ctx.replyWithPhoto(
-      new InputFile(resolvePath(import.meta.url, `../../assets/intro/${classType}.webp`)),
-      {
-        caption: ctx.i18n.t(`new.intro.${classType}`),
-        ...ctx.keyboards.mainMenu,
-      },
-    );
+  await createPlayer({
+    db: ctx.db,
+    data: {
+      userId: ctx.from.id,
+      name: ctx.from.username ?? 'Unnamed',
+      emoji,
+      gold: 0,
+      classType,
+      state: 'normal',
+    },
   });
 
-  return controller;
+  await ctx.replyWithPhoto(
+    new InputFile(resolvePath(import.meta.url, `../../assets/intro/${classType}.webp`)),
+    {
+      caption: ctx.i18n.t(`new.intro.${classType}`),
+      ...ctx.keyboards.mainMenu,
+    },
+  );
 }
+
+export const classController = new Composer<DefaultContext>();
+
+classController
+  .chatType('private')
+  .filter(hears('buttons.class.gnomes'), async ctx => handleClassSelection(ctx, 'gnome'));
+
+classController
+  .chatType('private')
+  .filter(hears('buttons.class.knights'), async ctx => handleClassSelection(ctx, 'knight'));
